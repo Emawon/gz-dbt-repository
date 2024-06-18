18f8f34def7fe5dc3fbb5017beb6f3ade4bba192
j.l.wang.v@gmail.com <j.l.wang.v@gmail.com>
2024-06-18 14:16:29 +0000

    add test  cha4.3

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -9,6 +9,8 @@ with renamed as(
       margin,
       shipping_fee,
       ship_cost + logcost as shipping_cost
+    -- ,round ( margin + shipping_fee - (ship_cost + logcost) , 2 ) as operational_margin 
+
     from {{ref('int_orders_margin')}}
     join {{ref('stg_raw__ship')}}
     using (orders_id)
